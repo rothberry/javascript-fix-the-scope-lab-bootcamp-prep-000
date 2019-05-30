604bd6bb6751446a499c2e14fc8b06800c1e2d27
Philip Roth <phil.roth077@gmail.com>
2019-05-30 23:21:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,20 +16,15 @@ function add2(n) {
   return n + 2
 
   // Feel free to move things around!
-
-}
-function funkyFunction() {
-  return "FUNKY!"
-
+  
 }
 
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
 
-// var funkyFunction = function() {
-//   return function() {
-//     return "FUNKY!"
-//   }
-// }
-//
-// // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// // NOTE: you only need to modify the code below this line.
-// var theFunk = funkyFunction
+// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
+// NOTE: you only need to modify the code below this line.
+var theFunk = funkyFunction
