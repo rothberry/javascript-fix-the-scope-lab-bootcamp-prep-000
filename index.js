5840c7edb3f5146098da194ceecf024266d782f6
Philip Roth <phil.roth077@gmail.com>
2019-05-30 23:20:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function add2(n) {
 }
 function funkyFunction() {
   return "FUNKY!"
-  
+
 }
 
 
@@ -29,7 +29,7 @@ function funkyFunction() {
 //     return "FUNKY!"
 //   }
 // }
-// 
+//
 // // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // // NOTE: you only need to modify the code below this line.
 // var theFunk = funkyFunction
